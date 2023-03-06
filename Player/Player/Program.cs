bcbd3915b4ab042d7bb2da26a7eba090feeab00c
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-03-06 18:44:09 +0500

    init

---
+++ Player/Player/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Palyers palyer = new Palyers(10,20,30);
+            palyer.Print();
+        }
+
+        class Palyers
+        {
+            private int _health = 100;
+            public int _speed;
+            public int _damage;
+
+            public Palyers(int health, int speed, int damage)
+            {
+                _health = health;
+                _speed = speed;
+                _damage = damage;
+            }
+
+            public Palyers()
+            {
+
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Жизнь = {_health} Speed = {_speed} Damage = {_damage}");
+            }
+        }
+    }
+}
